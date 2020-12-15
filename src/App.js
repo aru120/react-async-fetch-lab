dcab33451c365a239a3dde052bd43e31bbd2f148
aru120 <khuang159@gmail.com>
2020-12-14 20:50:58 -0500

    lab finished

--- src/App.js
+++ src/App.js
@@ -1,1 +1,34 @@
 // create your App component here
+import React from 'react'
+
+class App extends React.Component{
+
+    state={
+        space: []
+    }
+
+    componentDidMount(){
+        fetch("http://api.open-notify.org/astros.json")
+        .then(r => r.json())
+        .then((data) => this.setState({space: data}))
+    }
+
+    renderSpace = () =>{
+       return this.state.space.people.map(spaceMan => <h1 key={spaceMan}>{spaceMan.name}</h1>)
+    }
+
+    render(){
+        
+
+        return(
+            <React.Fragment>
+            {this.state.space.length === 0 ? <h1>Loading Spacemen</h1> :
+             <div>
+                {this.renderSpace()}
+            </div>}
+           </React.Fragment>
+        );
+    }
+}
+
+export default App
